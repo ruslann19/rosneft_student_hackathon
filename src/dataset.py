fix mIoU calculation

src/dataset.py
@@ -6,7 +6,7 @@ import torch
 from PIL import Image
 from torch.utils.data import Dataset
 
-VALUE2CLASS = {
+GRAY_TO_CLASS = {
     0: 0,
     25: 1,
     30: 2,
@@ -50,57 +50,58 @@ VALUE2CLASS = {
 }
 
 
+CLASS_TO_GRAY = {
+    0: 0,
+    1: 25,
+    2: 30,
+    3: 35,
+    4: 40,
+    5: 45,
+    6: 50,
+    7: 55,
+    8: 60,
+    9: 65,
+    10: 70,
+    11: 75,
+    12: 80,
+    13: 85,
+    14: 90,
+    15: 95,
+    16: 100,
+    17: 105,
+    18: 110,
+    19: 115,
+    20: 120,
+    21: 125,
+    22: 130,
+    23: 135,
+    24: 140,
+    25: 145,
+    26: 150,
+    27: 155,
+    28: 160,
+    29: 165,
+    30: 170,
+    31: 175,
+    32: 180,
+    33: 185,
+    34: 190,
+    35: 195,
+    36: 200,
+    37: 205,
+    38: 210,
+    39: 215,
+}
+
+
 class SegmentationDataset(Dataset):
     def __init__(self, img_dir, mask_dir):
         key = lambda p: int(p.stem)
         self.img_paths = sorted(Path(img_dir).glob("*"), key=key)
         self.mask_paths = sorted(Path(mask_dir).glob("*"), key=key)
 
-        self.gray_to_class = {
-            0: 0,
-            25: 1,
-            30: 2,
-            35: 3,
-            40: 4,
-            45: 5,
-            50: 6,
-            55: 7,
-            60: 8,
-            65: 9,
-            70: 10,
-            75: 11,
-            80: 12,
-            85: 13,
-            90: 14,
-            95: 15,
-            100: 16,
-            105: 17,
-            110: 18,
-            115: 19,
-            120: 20,
-            125: 21,
-            130: 22,
-            135: 23,
-            140: 24,
-            145: 25,
-            150: 26,
-            155: 27,
-            160: 28,
-            165: 29,
-            170: 30,
-            175: 31,
-            180: 32,
-            185: 33,
-            190: 34,
-            195: 35,
-            200: 36,
-            205: 37,
-            210: 38,
-            215: 39,
-        }
-
         self.lookup = np.zeros(256, dtype=np.int64)
-        for gray, cls in self.gray_to_class.items():
+        for gray, cls in GRAY_TO_CLASS.items():
             if 0 <= gray < 256:
                 self.lookup[gray] = cls
 
@@ -119,10 +120,3 @@ class SegmentationDataset(Dataset):
         y = mask
 
         return x, y
-
-
-if __name__ == "__main__":
-    dataset = SegmentationDataset(img_dir="data/input", mask_dir="data/target")
-
-# Использование:
-# loader = DataLoader(SegDataset("data/input", "data/target"), batch_size=4, shuffle=True)

src/trainer.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
+from src.dataset import CLASS_TO_GRAY
+
 
 class Trainer:
     def __init__(
@@ -47,6 +49,10 @@ class Trainer:
         self.train_metrics = []
         self.val_metrics = []
 
+        self.reverse_lookup = np.zeros(256, dtype=np.int64)
+        for gray, cls in CLASS_TO_GRAY.items():
+            self.reverse_lookup[cls] = gray  # индекс класса -> градация серого
+
     def _setup_logger(self):
         logger = logging.getLogger("Trainer")
         logger.setLevel(logging.INFO)
@@ -70,7 +76,10 @@ class Trainer:
             self.optimizer.step()
 
             total_loss += loss.item()
-            total_metrics += self.metrics(outputs, masks, self.num_classes)
+
+            preds = torch.argmax(outputs, dim=1).cpu().numpy()
+            masks = masks.cpu().numpy()
+            total_metrics += self.metrics(preds, masks, self.num_classes)
 
         avg_loss = total_loss / len(self.train_loader)
         avg_metrics = total_metrics / len(self.train_loader)
@@ -85,9 +94,11 @@ class Trainer:
             images, masks = images.to(self.device), masks.to(self.device)
             outputs = self.model(images)
             loss = self.criterion(outputs, masks)
-
             total_loss += loss.item()
-            total_metrics += self.metrics(outputs, masks, self.num_classes)
+
+            preds = torch.argmax(outputs, dim=1).cpu().numpy()
+            masks = masks.cpu().numpy()
+            total_metrics += self.metrics(preds, masks, self.num_classes)
 
         avg_loss = total_loss / len(self.val_loader)
         avg_metrics = total_metrics / len(self.val_loader)
@@ -118,15 +129,18 @@ class Trainer:
             mask = masks[i].cpu().numpy()
             pred = preds[i]
 
+            masks = self.reverse_lookup[masks]
+            preds = self.reverse_lookup[preds]
+
             axes[i, 0].imshow(img)
             axes[i, 0].set_title("Image")
             axes[i, 0].axis("off")
 
-            axes[i, 1].imshow(mask, cmap="tab20", vmin=0, vmax=self.num_classes - 1)
+            axes[i, 1].imshow(mask, cmap="gray", vmin=0, vmax=self.num_classes - 1)
             axes[i, 1].set_title("True Mask")
             axes[i, 1].axis("off")
 
-            axes[i, 2].imshow(pred, cmap="tab20", vmin=0, vmax=self.num_classes - 1)
+            axes[i, 2].imshow(pred, cmap="gray", vmin=0, vmax=self.num_classes - 1)
             axes[i, 2].set_title("Pred Mask")
             axes[i, 2].axis("off")
 